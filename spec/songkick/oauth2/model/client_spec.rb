bafe7132eb38697f1efb2426a703302b20c0f900
Adam Vaughan <adamjvaughan@gmail.com>
2015-06-18 07:12:45 -0600

    Fix gemspec dependency conflict, update rspec version and usage.

--- spec/songkick/oauth2/model/client_spec.rb
+++ spec/songkick/oauth2/model/client_spec.rb
@@ -8,50 +8,50 @@ describe Songkick::OAuth2::Model::Client do
   end
 
   it "is valid" do
-    @client.should be_valid
+    expect(@client).to be_valid
   end
 
   it "is invalid without a name" do
     @client.name = nil
-    @client.should_not be_valid
+    expect(@client).not_to be_valid
   end
 
   it "is invalid without a redirect_uri" do
     @client.redirect_uri = nil
-    @client.should_not be_valid
+    expect(@client).not_to be_valid
   end
 
   it "is invalid with a non-URI redirect_uri" do
     @client.redirect_uri = 'foo'
-    @client.should_not be_valid
+    expect(@client).not_to be_valid
   end
 
   # http://en.wikipedia.org/wiki/HTTP_response_splitting
   it "is invalid if the URI contains HTTP line breaks" do
     @client.redirect_uri = "http://example.com/c\r\nb"
-    @client.should_not be_valid
+    expect(@client).not_to be_valid
   end
 
   if (defined?(ActiveRecord::VERSION) && ActiveRecord::VERSION::MAJOR <= 3) || defined?(ProtectedAttributes)
     it "cannot mass-assign client_id" do
       @client.update_attributes(:client_id => 'foo')
-      @client.client_id.should_not == 'foo'
+      expect(@client.client_id).not_to eql('foo')
     end
 
     it "cannot mass-assign client_secret" do
       @client.update_attributes(:client_secret => 'foo')
-      @client.client_secret.should_not == 'foo'
+      expect(@client.client_secret).not_to eql('foo')
     end
   end
 
   it "has client_id and client_secret filled in" do
-    @client.client_id.should_not be_nil
-    @client.client_secret.should_not be_nil
+    expect(@client.client_id).not_to be_nil
+    expect(@client.client_secret).not_to be_nil
   end
 
   it "destroys its authorizations on destroy" do
     @client.destroy
-    Songkick::OAuth2::Model::Authorization.count.should be_zero
+    expect(Songkick::OAuth2::Model::Authorization.count).to be_zero
   end
 end
 
